20580bb980fed6c81dedfe4370da55f61999b65b
GabrielZuany <gzuanydev@gmail.com>
2024-09-06 00:41:17 -0300

    fix: The changes include modifying the main function in fsh.c to handle background execution and stdout redirection.

--- src/fsh.c
+++ src/fsh.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 #include "libs/lib.h"
 #include "libs/pmb.h"
 
@@ -19,26 +20,40 @@ int main(int argc, char *argv[]) {
 
             char **args = get_args_execvp(command);
 
-            if (!i) {
-                if (!pid) {
+            if (!i) { // if it's the first command
+                if (!pid) { // if it's the child
                     execvp(args[0], args);
                     exit(0);
                 }
                 commands++;
-                wait(NULL);
-                continue;
+                wait(NULL); // wait for the child to finish
+                continue; // skip the rest of the loop
+            }else{
+                // redirect the stdout to /dev/null for the second command and onwards
+                int fd = open("/dev/null", O_WRONLY);
+                dup2(fd, STDOUT_FILENO);
+                close(fd);
             }
-            
-            if (!pid) {
-                strcat(*commands, " &");
+
+            // second command and onwards will be run in the background and create another child
+            // remeber the fsh has a built-in bug! The processess create a child process that runs the same command
+            strcat(*commands, " &");
+            if (!pid) { // if it's the child
+                // strcat(*commands, " &");
                 execvp(args[0], args);
-                // create another child and run the same command
                 exit(0);
             }
+            // if it's the parent
+            system(*commands);
             commands++;
 
             wait(NULL);
         }
+
+        // reset the stdout
+        int fd = open("/dev/tty", O_WRONLY);
+        dup2(fd, STDOUT_FILENO);
+        close(fd);
     }
 
     return 0;
