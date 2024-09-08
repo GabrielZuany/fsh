4cc75b91baed2b17702a2e4ec4f88ff81c921b4d
GabrielZuany <gzuanydev@gmail.com>
2024-09-08 00:14:42 -0300

    feat: processes running in background

--- src/fsh.c
+++ src/fsh.c
@@ -32,12 +32,23 @@ void handle_sigint(int signum) {
     scanf("%c", &c);
     if (c == 'y') {
         printf("Exiting...\n");
-        // kill_all_processes(global_pmb, signum);
+        kill_all_processes(global_pmb, signum);
         exit(0);
     }
     printf("Continuing...\n");
     printf("fsh> ");
     fflush(stdout);
+    return;
+}
+
+void handle_child_signal(int signum) {
+    printf("(child) Signal %d received\n", signum);
+    int status;
+    pid_t pid = waitpid(-1, &status, WNOHANG);
+    if (pid > 0) {
+        printf("(child) Process %d finished\n", pid);
+        fflush(stdout);
+    }
     return;
 }
 
@@ -47,12 +58,17 @@ int main(int argc, char *argv[]) {
     signal(SIGSTOP, handle_signal); // Suspende
     signal(SIGCONT, handle_sigcont); // Continua
 
+    global_pmb = create_process_map_block();
     while (true) {
+        // clear_process_map_block(global_pmb);
         printf("fsh> ");
         char **commands = read_commands_stdin();
-        global_pmb = create_process_map_block();
-        for (int i = 0; *commands; i++) {
+        int i = 0;
+        pid_t first_pid = 0;
+        for (i = 0; *commands; i++) {
             pid_t pid = fork();
+            if (!i) {  first_pid = pid; }
+
             add_process_to_map_block(global_pmb, pid, *commands);
             char *command = malloc(strlen(*commands) + 1);
             strcpy(command, *commands);
@@ -66,44 +82,32 @@ int main(int argc, char *argv[]) {
                     exit(0);
                 }
                 commands++;
-                wait(NULL); // wait for the child to finish
                 continue; // skip the rest of the loop
-            } else {
-                // redirect the stdout to /dev/null for the second command and onwards
-                int fd = open("/dev/null", O_WRONLY);
-                dup2(fd, STDOUT_FILENO);
-                close(fd);
             }
 
             // second command and onwards will be run in the background and create another child
             // remeber the fsh has a built-in bug! The processess create a child process that runs the same command
-            if (!pid) { // if it's the child
-                // strcat(*commands, " &");
-                if (execvp(args[0], args) < 0)
-                    printf("fsh: command not found\n");
-
+            if (!pid) { // if it's the child (fsh 2nd/3rd/4th/5th child)
                 pid_t bug_pid = fork();
-                if (!bug_pid) {
-                    if (execvp(args[0], args) < 0)
+                printf("Bug pid created: %d\n", bug_pid);
+                add_process_to_map_block(global_pmb, bug_pid, *commands); // global_pmb should be a shared variable
+                if (!bug_pid) { // if it's the (bug) child
+                    if (execvp(args[0], args) < 0){
                         printf("fsh: command not found\n");
+                    }
+                    // fflush(stdout);
                     exit(0);
+                }       
+                printf("Parent process %d waiting for bug process %d\n----\n", getpid(), bug_pid);
+                if (execvp(args[0], args) < 0){
+                    printf("fsh: command not found\n");       
                 }
+                // fflush(stdout);
                 exit(0);
             }
-            // if it's the parent
-            system(*commands);
             commands++;
-
-            // theoretically, for the process to run in background, the parent shouldn't wait for the child
-            // dunno
-            /* wait(NULL); */
         }
-
-        // reset the stdout
-        int fd = open("/dev/tty", O_WRONLY);
-        dup2(fd, STDOUT_FILENO);
-        close(fd);
-        clear_process_map_block(global_pmb);
+        waitpid(first_pid, NULL, WNOHANG); // fsh waits for the child to finish
         fflush(stdout);
     }
     destroy_process_map_block(global_pmb);
