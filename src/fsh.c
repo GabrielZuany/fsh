322be4167d14fca6bb25fb2596812209d6b91ead
GabrielZuany <gzuanydev@gmail.com>
2024-09-09 17:44:32 -0300

    chore: free some resources

--- src/fsh.c
+++ src/fsh.c
@@ -33,6 +33,7 @@ void handle_sigint(int signum) {
     if (c == 'y') {
         printf("Exiting...\n");
         kill_all_processes(global_pmb, signum);
+        destroy_process_map_block(global_pmb);
         exit(0);
     }
     printf("Continuing...\n");
@@ -65,16 +66,18 @@ int main(int argc, char *argv[]) {
     }
 
     global_pmb = create_process_map_block();
+    char **commands;
     /* add_process_to_map_block(global_pmb, getpid(), "fsh"); */
     while (true) {
         printf("fsh> ");
-        char **commands = read_commands_stdin();
+        commands = read_commands_stdin();
         if (strcmp(*commands, "waitall") == 0) {
             while (wait(NULL) > 0);
             continue;
         }
         if (strcmp(*commands, "die") == 0) {
             kill_all_processes(global_pmb, SIGTERM);
+            free(commands);
             /* add_process_to_map_block(global_pmb, getpid(), "fsh"); */
             continue;
         }
@@ -142,7 +145,6 @@ int main(int argc, char *argv[]) {
             commands++;
         }
         waitpid(first_pid, NULL, 0); // fsh waits for the child to finish
-
         // reset the stdout
         int fd = open("/dev/tty", O_WRONLY);
         dup2(fd, STDOUT_FILENO);
@@ -152,6 +154,5 @@ int main(int argc, char *argv[]) {
     }
     kill_all_processes(global_pmb, SIGTERM);
     destroy_process_map_block(global_pmb);
-
     return 0;
 }
